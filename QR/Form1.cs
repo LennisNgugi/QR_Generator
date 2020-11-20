f1c579fc11f9188741f3fb3ce52ceda68f4fc6f7
734415 <42470457+LennisNgugi@users.noreply.github.com>
2020-11-20 22:16:54 +0000

    Add files via upload

---
+++ QR/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QR.QRGenerator QG = new QR.QRGenerator();
+            var MyData = QG.CreateQrCode(textBox1.Text, QR.QRGenerator.ECCLevel.H);
+            var code = new QR.QRCode(MyData);
+            pictureBox1.Image = code.GetGraphic(50);
+
+        }
+    }
+}
